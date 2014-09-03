6fc8a0de711bfe08e9ce149d797cb0ed9e695743
madplay <madplay@naver.com>
2014-09-03 17:51:25 +0900

    [Generator] Table Reader for framework is created.

---
+++ Generator/src/TableReader.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+
+public class TableReader {
+	FileInputStream tableFile;
+	BufferedReader tableReader;
+	String xmlPath; // XML file path
+	String javaPath; // java file path
+	
+	/**
+	 * Table Reader Constructor
+	 * Set and Initialize variable
+	 * @param tablePath
+	 * @param xmlPath
+	 * @param javaPath
+	 */
+	public TableReader(String tablePath, String xmlPath, String javaPath) {
+		try {
+			if((tableFile = new FileInputStream(tablePath)) == null) {
+				// Error : tableFile not Found
+				return;
+			}
+			this.xmlPath = xmlPath;
+			this.javaPath = javaPath;
+			tableReader = new BufferedReader(new InputStreamReader(tableFile,"UTF-8"));
+			
+			/*
+			 * need function for readTable
+			 * readTable();
+			 */
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * read Table for XML and Java generate
+	 * modifying... 2014.09.03
+	 */
+	public void readTable() {
+		
+	}
+}

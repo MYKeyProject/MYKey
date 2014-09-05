8eb5d804733239b888e50ad1f36c7ec270406714
madplay <madplay@naver.com>
2014-09-05 20:53:07 +0900

    [Generator] modify readKeyArray method(add key instantiation)

--- Generator/src/generator/xml/XMLGenerator.java
+++ Generator/src/generator/xml/XMLGenerator.java
@@ -18,10 +18,9 @@ public class XMLGenerator {
 	int maxCol = 0;
 	double verticalRate = 0.0;
 	double horizontalRate = 100.0;
-	
+
 	ArrayList<Key> keyArray[];
-	
-	
+
 	/**
 	 * XMLGenerator Constructor.
 	 */
@@ -33,31 +32,78 @@ public class XMLGenerator {
 		this.maxRow = maxRow;
 		this.maxCol = maxCol;
 		this.verticalRate = verticalRate;
-		
+
 		keyArray = new ArrayList[numOfRow];
-		
+
 		try {
 			for (int idx = 0; idx < keyArray.length; idx++) {
 				keyArray[idx] = new ArrayList<Key>();
 			}
-			XMLFile = new FileOutputStream(xmlPath + File.separator + "keyboard.xml");
-			XMLFileWriter = new BufferedWriter(new OutputStreamWriter(XMLFile, "UTF-8"));
-			
-			
+			XMLFile = new FileOutputStream(xmlPath + File.separator
+					+ "keyboard.xml");
+			XMLFileWriter = new BufferedWriter(new OutputStreamWriter(XMLFile,
+					"UTF-8"));
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void readKeyArray(String layoutInfo) {
+		String[] readLine = null;
+		String[] tmpStr = null;
+		String[] tmpKeyCode = null;
+		int rowCount = 0; // row counting variable
+
+		// attribute of each key.
+		int[] keyCode = null;
+		int keyWidth = 0;
+		int keyHeight = 0;
+
+		String keyLabel = null; 
+		String keyIcon = null;
+		int keyEdgeFlags = 0;
+		int isRepeatable = 0;
+
+		try {
+			readLine = layoutInfo.split("\n");
+
+			for (int idx = 0; idx < readLine.length; idx++) {
+				tmpStr = readLine[idx].split("\t");
+				tmpKeyCode = tmpStr[0].split(",");
+				keyCode = new int[tmpKeyCode.length];
+				for (int numOfKeyCode = 0; numOfKeyCode < tmpKeyCode.length; numOfKeyCode++) {
+					keyCode[numOfKeyCode] = Integer
+							.parseInt(tmpKeyCode[numOfKeyCode]);
+				}
+				keyWidth = Integer.parseInt(tmpStr[1]);
+				keyHeight = Integer.parseInt(tmpStr[2]);
+
+				keyLabel = tmpStr[3];
+				keyIcon = tmpStr[4];
+
+				keyEdgeFlags = Integer.parseInt(tmpStr[5]);
+				isRepeatable = Integer.parseInt(tmpStr[6]);
+
+				rowCount = Integer.parseInt(tmpStr[7]);
+
+				// instantiation
+				Key keyInfo = new Key(keyCode, keyWidth, keyHeight, keyLabel,
+						keyIcon, keyEdgeFlags, isRepeatable);
+				
+				keyArray[rowCount-1].add(keyInfo);
+			}
+			makeXML();
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public void readKeyArray() {
-		// read and make key array list
-	}
-	
+
 	public void writeKeyLayoutInfo() {
 		// write layout information
 	}
-	
+
 	public void makeXML() {
 		// XML File Creator
 	}

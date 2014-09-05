8897880d60a5123ccbe526f07326d3ee4a24b021
madplay <madplay@naver.com>
2014-09-05 15:11:47 +0900

    [Generator] XML Generator Constructor, readKeyArray method, makeXML method, writeKeyLayoutInfo method. but it don't any function.

--- Generator/src/generator/xml/XMLGenerator.java
+++ Generator/src/generator/xml/XMLGenerator.java
@@ -1,20 +1,64 @@
 package generator.xml;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+
+import template.Key;
 
 public class XMLGenerator {
 	BufferedWriter XMLFileWriter;
 	FileOutputStream XMLFile;
+
+	int numOfKey = 0;
+	int numOfRow = 0;
+	int maxRow = 0;
+	int maxCol = 0;
+	double verticalRate = 0.0;
+	double horizontalRate = 100.0;
+	
+	ArrayList<Key> keyArray[];
+	
 	
 	/**
 	 * XMLGenerator Constructor.
 	 */
-	public XMLGenerator() {
+	@SuppressWarnings("unchecked")
+	public XMLGenerator(int numOfKey, int numOfRow, int maxRow, int maxCol,
+			double verticalRate, String xmlPath) {
+		this.numOfKey = numOfKey;
+		this.numOfRow = numOfRow;
+		this.maxRow = maxRow;
+		this.maxCol = maxCol;
+		this.verticalRate = verticalRate;
+		
+		keyArray = new ArrayList[numOfRow];
+		
 		try {
-
+			for (int idx = 0; idx < keyArray.length; idx++) {
+				keyArray[idx] = new ArrayList<Key>();
+			}
+			XMLFile = new FileOutputStream(xmlPath + File.separator + "keyboard.xml");
+			XMLFileWriter = new BufferedWriter(new OutputStreamWriter(XMLFile, "UTF-8"));
+			
+			
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public void readKeyArray() {
+		// read and make key array list
+	}
+	
+	public void writeKeyLayoutInfo() {
+		// write layout information
+	}
+	
+	public void makeXML() {
+		// XML File Creator
 	}
 }
